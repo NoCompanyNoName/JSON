41529f33b97417c6225e41339c321ab3bb006231
YGnezdilova <126925525+YGnezdilova@users.noreply.github.com>
2023-04-23 20:42:23 +0300

    Add files via upload

--- JSONPath_in_Action/src/test/java/TestUsers.java
+++ JSONPath_in_Action/src/test/java/TestUsers.java
@@ -1,73 +1,129 @@
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
 import org.junit.jupiter.api.Assertions;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import static io.restassured.RestAssured.given;
 import io.restassured.response.Response;
-
-import java.util.Collections;
 import java.util.List;
 
 
 public class TestUsers {
+    @BeforeTest
+    public void baseURI() {
+        RestAssured.baseURI = "https://jsonplaceholder.typicode.com";
+    }
+
     @Test
-    public void sendGetRequest() {
-        RestAssured.baseURI ="https://jsonplaceholder.typicode.com";
+    public void getEmail() {
         Response response = given().
                 contentType(ContentType.JSON).
                 when().
                 get("users").
                 then().extract().response();
         Assertions.assertEquals(200, response.statusCode());
-        System.out.println("Emails are "+response.jsonPath().getList("email"));
-        System.out.println("Zipcodes are "+response.jsonPath().getList("address.zipcode"));
-        List<Object> zipCodes = response.jsonPath().getList("address.zipcode");
-       for(int i = 0; i < zipCodes.size(); i++) {
-           if (zipCodes.get(i).toString().length()==5) {
-               System.out.println("Zipcode without dash-symbol is  "+zipCodes.get(i));
-           }
-       }
+        System.out.println("Emails are " + response.jsonPath().getList("email"));
+    }
+
+    @Test
+    public void getZipCode() {
+        Response response = given().
+                contentType(ContentType.JSON).
+                when().
+                get("users").
+                then().extract().response();
+        Assertions.assertEquals(200, response.statusCode());
+        System.out.println("Zipcodes are " + response.jsonPath().getList("address.zipcode"));
+    }
+
+    @Test
+    public void getZipCodeWithoutDash() {
+        Response response = given().
+                contentType(ContentType.JSON).
+                when().
+                get("users").
+                then().extract().response();
+        Assertions.assertEquals(200, response.statusCode());
+        JsonPath jsonPath = response.jsonPath();
+        System.out.println("Zipcode without dash-symbol is  " + jsonPath.getList("findAll{!it.address.zipcode.contains('-')}.address.zipcode"));
+    }
+
+    @Test
+    public void getLatAndLng() {
+        Response response = given().
+                contentType(ContentType.JSON).
+                when().
+                get("users").
+                then().extract().response();
+        Assertions.assertEquals(200, response.statusCode());
         List<Object> names = response.jsonPath().getList("name");
         List<Object> geoLat = response.jsonPath().getList("address.geo.lat");
         List<Object> geoLng = response.jsonPath().getList("address.geo.lng");
-        for(int i = 0; i < names.size(); i++) {
-           System.out.println(names.get(i)+" is situated at: lat = "+geoLat.get(i)+" and lng = "+geoLng.get(i));
+        for (int i = 0; i < names.size(); i++) {
+            System.out.println(names.get(i) + " is situated at: lat = " + geoLat.get(i) + " and lng = " + geoLng.get(i));
         }
-        List<Object> usernames = response.jsonPath().getList("username");
-
-        for(int i = 0; i < usernames.size(); i++) {
-            if((geoLat.get(i).toString().contains("-")) && (geoLng.get(i).toString().contains("-"))) {
-                System.out.println(usernames.get(i) + "has negative lat and lng.");
-            }
+    }
+    @Test
+        public void getNegativeLatAndLng() {
+    Response response = given().
+            contentType(ContentType.JSON).
+            when().
+            get("users").
+            then().extract().response();
+    Assertions.assertEquals(200, response.statusCode());
+        JsonPath jsonPath = response.jsonPath();
+        System.out.println("Users with negative Lat and Lng are " + jsonPath.getList("findAll{(it.address.geo.lat.contains('-'))&&(it.address.geo.lng.contains('-'))}.username"));
+    }
+    @Test
+    public void getEndOfWebsite() {
+        Response response = given().
+                contentType(ContentType.JSON).
+                when().
+                get("users").
+                then().extract().response();
+        Assertions.assertEquals(200, response.statusCode());
+        JsonPath jsonPath = response.jsonPath();
+        List<Object> names = jsonPath.getList("findAll{it.website.endsWith('.info')}.name");
+        List<Object> webpage = jsonPath.getList("findAll{it.website.endsWith('.info')}.website");
+        for (int i = 0; i < names.size(); i++) {
+            System.out.println(names.get(i)+" has webpage "+ webpage.get(i));
         }
-        List<Object> websites = response.jsonPath().getList("website");
-        for(int i = 0; i < usernames.size(); i++) {
-            if((websites.get(i).toString().contains(".info"))) {
-                System.out.println(usernames.get(i) + " website contain info.");
-            }
-        }
-        List<Long> geoLng_1 = response.jsonPath().getList("address.geo.lng");
-        int index =geoLng_1.indexOf(Collections.max(geoLng_1));
-        System.out.println(names.get(index)+ " this is user with max lng");
+    }
+    @Test
+    public void getLngMax() {
+        Response response = given().
+                contentType(ContentType.JSON).
+                when().
+                get("users").
+                then().extract().response();
+        Assertions.assertEquals(200, response.statusCode());
+        JsonPath jsonPath = response.jsonPath();
+        System.out.println("User with max Lng is " + jsonPath.getString("max{it.address.geo.lng}.name"));
 
-        List<Object> catchPhrases = response.jsonPath().getList("company.catchPhrase");
-        int longestValue = 0;
-        int indexOfLongestValue = 0;
-        for(int i = 0; i < names.size(); i++) {
-            if(catchPhrases.get(i).toString().length()>longestValue) {
-                longestValue=catchPhrases.get(i).toString().length();
-                indexOfLongestValue=i;
-            }
-            }
-        System.out.println(names.get(indexOfLongestValue)+ " this is user with longest catchPhrase");
-        }
+    }
 
-
-
-
+    @Test
+        public void getLongestCatchPhrase() {
+        Response response = given().
+                contentType(ContentType.JSON).
+                when().
+                get("users").
+                then().extract().response();
+        Assertions.assertEquals(200, response.statusCode());
+        JsonPath jsonPath = response.jsonPath();
+        System.out.println("User with longest CatchPhrase is " + jsonPath.getString("max{it.company.catchPhrase.size()}.name"));
 
 
     }
+
+
+
+}
+
+
+
+
 
 
 
